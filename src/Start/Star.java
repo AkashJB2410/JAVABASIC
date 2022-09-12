e39415fd43bf969c841e4368012fb9ca11d3917b
AkashJB2410 <akashbhandare23@gmail.com>
2022-09-12 18:23:03 +0530

    aaa

--- src/Start/Star.java
+++ src/Start/Star.java
@@ -2,12 +2,15 @@ package Start;
 
 public class Star {
     public static void main(String[] args) {
-        for (int i = 0; i < 6 ; i++) {
-            for (int j = 5; j >= i; j--) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <=i; j++) {
-                System.out.print("* ");
+        for (int k = 0; k <= 5; k++) {
+            for (int i = 0; i < 6 ; i++) {
+                for (int j = 5; j >= i; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <=i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
