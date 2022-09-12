7bda0360fdd60208383ffa5b581f11d99a532469
AkashJB2410 <akashbhandare23@gmail.com>
2022-09-12 18:36:08 +0530

    123
    Signed-off-by: AkashJB2410 <akashbhandare23@gmail.com>

---
+++ src/Akash/AJ.java
@@ -0,0 +1,7 @@
+package Akash;
+
+public class AJ {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO JAVA");
+    }
+}
---
+++ src/Start/AB.java
@@ -0,0 +1,7 @@
+package Start;
+
+public class AB {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO JAVA");
+    }
+}
