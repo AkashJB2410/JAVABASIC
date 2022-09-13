98dc151965330eba6859d26848514ec64c941611
AkashJB2410 <akashbhandare23@gmail.com>
2022-09-13 15:28:36 +0530

    Modified

--- src/Start/DATETIME.java
+++ src/Start/DATETIME.java
@@ -10,12 +10,12 @@ public class DATETIME {
     public static <DateAndTime> void main(String[] args) throws ParseException {
         System.out.println("DATE AMD TIME");
         String sDate1 = "2022-10-24-23:59:59";
+        System.out.println(sDate1);
         char ch=' ';
         String b = "";
         for (int i = 0; i < sDate1.length() ; i++) {
             char a = sDate1.charAt(i);
             if (sDate1.charAt(i)=='-'&i==10){
-                sDate1.replace('-',' ');
                 b=b+ch;
             }
             else {
