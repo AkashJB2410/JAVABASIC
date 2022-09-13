3e79f4b0bb4bead80c2c5faca990feb0910df33b
AkashJB2410 <akashbhandare23@gmail.com>
2022-09-13 12:52:26 +0530

    123
    Signed-off-by: AkashJB2410 <akashbhandare23@gmail.com>

---
+++ src/Akash/PrimeNumber.java
@@ -0,0 +1,12 @@
+package Akash;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int number ;
+
+//        while
+//        do {
+//            System.out.println(number);
+//        }
+    }
+}
---
+++ src/Start/DATETIME.java
@@ -0,0 +1,30 @@
+package Start;
+
+import java.io.InterruptedIOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+//import java.time.Instant;
+//import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+
+public class DATETIME {
+
+    public static <DateAndTime> void main(String[] args) throws ParseException {
+        String sDate1 = "2022-10-24-23:59:59";
+        char ch=' ';
+        String b = "";
+        for (int i = 0; i < sDate1.length() ; i++) {
+            char a = sDate1.charAt(i);
+            if (sDate1.charAt(i)=='-'&i==10){
+                sDate1.replace('-',' ');
+                b=b+ch;
+            }
+            else {
+                b=b+a;
+            }
+        }
+        SimpleDateFormat s1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = s1.parse(b);
+        System.out.println(b+"\n"+date);
+    }
+}
